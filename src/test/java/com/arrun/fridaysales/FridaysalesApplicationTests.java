fc8887645e9fd1b8a57b8648e915048c01ec8a11
Arrun1219 <arunkumarbe12@gmail.com>
2026-05-23 16:45:02 +0800

    Update FridaysalesApplicationTests.java

--- src/test/java/com/arrun/fridaysales/FridaysalesApplicationTests.java
+++ src/test/java/com/arrun/fridaysales/FridaysalesApplicationTests.java
@@ -5,7 +5,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class FridaysalesApplicationTests {
-\\Commit add fdgsdff
+\\Commit add fdgsdff last commit
 	@Test
 	void contextLoads() {
 	}
